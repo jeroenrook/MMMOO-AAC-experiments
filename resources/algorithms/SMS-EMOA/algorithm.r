f46d2b2bbbb81735b5f31ce855853c1dc5341717
J. Rook <jrook94@gmail.com>
2021-12-20 13:00:04 +0100

    Add option to save solution set to file

--- resources/algorithms/SMS-EMOA/algorithm.r
+++ resources/algorithms/SMS-EMOA/algorithm.r
@@ -3,6 +3,7 @@
 library(optparse)
 library(smoof)
 library(ecr)
+library(ecr3vis)
 
 # ARGUMENTS
 # ===
@@ -10,6 +11,7 @@ option_list = list(
   make_option("--instance", type = "character", default = NULL, help = "instance"),
   make_option("--budget", type = "numeric", default = 10000L, help = "The maximum number of allowed function evaluations"),
   make_option("--seed", type = "numeric", default = 0, help = "The random seed"),
+  make_option("--save_solution", type= "character", default = NULL, "save solution set to an Rdata object"),
   #Add parameters here
   make_option("--mu", type = "numeric", default=100L),
   make_option("--mutator", type = "character", default = "mutPolynomial", help = "[mutGauss, mutPolynomial, mutUniform]"),
@@ -84,3 +86,18 @@ writeLines(paste("c EVALUATIONS", smoof::getNumberOfEvaluations(obj.fn)))
 # Parse the solution set to a common interface
 writeLines("s SOLUTION SET")
 print(optimizer$pareto.front)
+if (!is.null(opt$save_solution)){
+    writeLines("Save to file")
+    pareto_front <- optimizer$pareto.front
+    save(pareto_front, file=opt$save_solution)
+}
+
+#Compute the performance metrics
+#HV
+pareto.matrix <- t(data.matrix(optimizer$pareto.front))
+
+print(ecr3vis::hv(pareto.matrix, smoof::getRefPoint(obj.fn)))
+#IDG+
+#print(ecr3vis::igdp(pareto.matrix))
+#SP
+print(ecr3vis::solow_polasky(pareto.matrix))
