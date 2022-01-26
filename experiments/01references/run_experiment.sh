596de106d21418cd3d4653ebab3c475936b11244
J. Rook <jrook94@gmail.com>
2022-01-26 03:14:26 +0100

    Experiments to run configurations with fixed populations. Configuration is now capped at 250 runs with a sparkle file hack.

--- experiments/01references/run_experiment.sh
+++ experiments/01references/run_experiment.sh
@@ -2,4 +2,6 @@
 
 ./compute_references.R
 chmod 755 script.sh
-sbatch script.sh
+jobid=$(sbatch --parsable script.sh)
+echo "script launched with ${jobid}"
+sbatch --dependency=afterany:${jobid} ./analyse_results.R
---
+++ experiments/06validation/run_experiment.py
@@ -0,0 +1,1 @@
+#!/u`r/bin/env python3
